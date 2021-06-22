20ee9014cb1a0e967f2ca0c03da7c5d0208e204f
Roosa Vuotso <roosavuotso@Roosa-MacBook-Air.local>
2021-06-22 19:25:58 +0200

    This is my first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+let sugar = 80
+let butter = 120
+let flour = 240
+console.log(''Sugar:'', sugar, ''butter:'', butter, ''flour:'', flour)
+let mediumBowl = sugar + butter
+mediumBowl += flour
+
+console.log(''The content of my medium bowl is now:'', mediumBowl)
